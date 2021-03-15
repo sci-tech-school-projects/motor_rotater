6469a304d5642ac3c785a68433dc5fa071473e0b
Yasuyuki Arakawa <b.a520y@gmail.com>
2021-03-15 12:11:37 +0900

    before main.py ammendment for 3 cameras

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import cv2
 import numpy as np
-import RPi.GPIO as GPIO
 import sys, os, glob, time
 from datetime import datetime
+from dc_motor import Dc_motor
 
 
 class Motor_rotater():
@@ -13,82 +13,104 @@ class Motor_rotater():
     start_time = 0
     time = 0
     head_num = 0
+    rotate_speed = 50
+    limit_time = 180
 
     def __init__(self):
         print('***** init {}'.format(self.__class__.__name__))
         self.cap1 = cv2.VideoCapture(0)
-        self.cap2 = cv2.VideoCapture(2)
+
         self.get_file_length()
         self.create_file_name()
         self.make_dir()
 
     def get_file_length(self):
-        folders = glob.glob('imgs/*')
+        folders = glob.glob('imgs/*_')
         length = len(folders)
         self.head_num = str('{:0=4}'.format(length))
-        # print(self.head_num)
-        # print(type(self.head_num))
-        # sys.exit()
 
     def create_file_name(self):
-        print('***** args   [1]:shape [2]:color [3]:X [4]:Y [5]:Z ')
+        print('***** args   [1]:color [2]:shape [3]:X [4]:Y [5]:Z ')
         base_name = ''
         for i in range(1, 6):
             try:
-                base_name = base_name + '_' + sys.argv[i]
+                if i == 1:
+                    base_name = sys.argv[i]
+                else:
+                    base_name = base_name + '__' + sys.argv[i]
             except IndexError:
                 print('***** input correct arguments')
                 sys.exit()
-        self.base_name = base_name
+        self.base_name = self.head_num + '__' + base_name
 
     def make_dir(self):
-        dir_name = self.head_num + '_' + self.base_name
+        # dir_name = self.head_num + '__' + self.base_name
+        dir_name = self.base_name
         self.path = os.path.join('imgs', dir_name)
         if not os.path.exists(self.path):
             self.path = self.path + '_'
-            print("***** your face is to be saved here {} ".format(self.base_name))
+            print("***** images to be saved here {} ".format(self.base_name))
             os.mkdir(self.path)
 
-    def main(self):
-        counter = 0
+    def main(self, brightness):
+        file_name_counter = 0
         start_time = datetime.now()
-        time = 0
-        dc_motor = Dc_motor()
+        current_time = 0
+        dc_motor = Dc_motor(self.rotate_speed)
         dc_motor.rotate('start')
-        while time <= 10:
-            frame = None
-            if counter % 2 == 0:
+        active_cam = None
+
+        while current_time <= self.limit_time:
+            if current_time <= self.limit_time / 2:
+                active_cam = 'cap1'
                 ret, frame = self.cap1.read()
             else:
+                if active_cam == 'cap1':
+                    active_cam = 'cap2'
+                    file_name_counter = 0
+                    self.cap1.release()
+                    try:
+                        self.cap2 = cv2.VideoCapture(2)
+                    except Exception:
+                        sys.exit()
                 ret, frame = self.cap2.read()
 
-            frame = self.resize_img(frame)
+            if ret:
+                frame = self._trim_img(frame)
+                frame = self._adjust(frame, brightness, 0.0)
 
-            cv2.imshow('frame', frame)
-            self._save_img(frame, counter)
-            if cv2.waitKey(1) & 0xff == ord("q"):
+                # cv2.imshow('frame', frame)
+                self._save_img(frame, file_name_counter, active_cam)
+                file_name_counter += 1
+                current_time = self._timer(start_time)
+
+                if cv2.waitKey(1) & 0xff == ord("q"):
+                    break
+            else:
                 break
 
-            counter += 1
-            time = self._timer(start_time)
-
         dc_motor.rotate('stop')
+        self.cap2.release()
+        cv2.destroyAllWindows()
 
-    def resize_img(self, frame):
+    def _trim_img(self, frame):
         shape = np.shape(frame)
         h, w, c = shape
+        trim_rate = 5
 
-        start_x = int(w / 5)
-        start_y = int(h / 5)
-        end_x = int(w / 5) * 4
-        end_y = int(h / 5) * 4
+        start_x = int(w / trim_rate)
+        start_y = int(h / trim_rate)
+        end_x = w
+        end_y = h
 
-        frame = frame[start_y:end_y, start_x:end_x, ::]
+        return frame[start_y:end_y, start_x:end_x, ::]
 
-        return frame
-
-    def _save_img(self, frame, counter):
-        num = '{:0=4}'.format(counter)
+    def _save_img(self, frame, file_name_counter, active_cam):
+        num = None
+        if active_cam == 'cap1':
+            num = '{:0=4}'.format(file_name_counter)
+        elif active_cam == 'cap2':
+            num = '2{:0=3}'.format(file_name_counter)
         _path_file = os.path.join(self.path, self.base_name)
         path_file = _path_file + '_' + num + '.jpg'
         print(path_file)
@@ -99,47 +121,18 @@ class Motor_rotater():
         time = time.seconds
         return time
 
+    def _adjust(self, img, alpha=1.0, beta=0.0):
 
-class Dc_motor():
-    enA = 26
-    in1 = 19
-    in2 = 13
-    led = 15
-
-    key = None
-
-    def __init__(self):
-        print('***** init {}'.format(self.__class__.__name__))
-
-        GPIO.setmode(GPIO.BCM)
-        GPIO.setup(self.in1, GPIO.OUT)
-        GPIO.setup(self.in2, GPIO.OUT)
-        GPIO.setup(self.enA, GPIO.OUT)
-        GPIO.setup(self.led, GPIO.OUT)
-
-        GPIO.output(self.in1, GPIO.LOW)
-        GPIO.output(self.in2, GPIO.LOW)
-        GPIO.output(self.led, GPIO.HIGH)
-        self.p = GPIO.PWM(self.enA, 100)
-        self.p.start(100)
-
-    def rotate(self, key=None):
-
-        if key == 'start':
-            print("rotate")
-            GPIO.output(self.in1, GPIO.HIGH)
-            GPIO.output(self.in2, GPIO.LOW)
-
-        elif key == 'stop':
-            print("stop")
-            GPIO.output(self.in1, GPIO.LOW)
-            GPIO.output(self.in2, GPIO.LOW)
-            self.release_gpio()
-
-    def release_gpio(self):
-        GPIO.cleanup()
+        # 積和演算を行う。
+        dst = alpha * img + beta
+        # [0, 255] でクリップし、uint8 型にする。
+        return np.clip(dst, 0, 255).astype(np.uint8)
 
 
 if __name__ == '__main__':
+    try:
+        brightness = float(sys.argv[6])
+    except:
+        brightness = 1.2
     motor_rotater = Motor_rotater()
-    motor_rotater.main()
+    motor_rotater.main(brightness)
